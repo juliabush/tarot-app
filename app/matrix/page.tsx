93a8bbbadaeaa7826468499b5976ebb899e68884
Julia Bush <j.elizabethbush@gmail.com>
2025-11-17 20:12:16 +0100

    page for destiny matrixes in the future

---
+++ app/matrix/page.tsx
@@ -0,0 +1,8 @@
+export default function DestinyMatrixPage() {
+  return (
+    <div className="min-h-screen flex flex-col pb-36 items-center justify-center">
+      <h1 className="text-4xl font-bold mb-4">Destiny matrix</h1>
+      <p>Destiny matrix calculator coming soon!</p>
+    </div>
+  );
+}
