d60967c20c37bfdf721e6e23af5be6331246a0fa
Julia Bush <j.elizabethbush@gmail.com>
2025-11-22 13:29:27 +0100

    types for next auth

---
+++ next-auth.d.ts
@@ -0,0 +1,17 @@
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    } & DefaultSession["user"];
+  }
+
+  interface User extends DefaultUser {
+    id: string;
+    avatar?: string | null;
+  }
+}
